41cdf896544a1408cee0f156f7470d1ef17599f8
fahadwasim <fahad.waseem102@gmail.com>
2025-10-17 02:19:23 +0500

    joker card added in chaos

--- lib/controllers/chaos_controller.dart
+++ lib/controllers/chaos_controller.dart
@@ -4,7 +4,6 @@ import '../view/global/session_manager.dart';
 
 class ChaosController extends GetxController {
   RxBool chaosActive = true.obs;
-
   @override
   void onInit() {
     super.onInit();
